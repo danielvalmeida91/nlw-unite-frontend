368fef72c0dd8a8845ac977d28a37f5701541f74
danielvalmeida91 <danielvalmeida91@gmail.com>
2024-04-03 23:58:39 -0300

    Aula 01 finished

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,9 +2,9 @@ import nlwUniteLogo from '../assets/nlw-unite-icon.svg'
 export function Header(){
 
   return (
-    <div className='flex gap-5 items-center py-2'>
+    <div className='flex items-center gap-5 py-2'>
       <img src={nlwUniteLogo} alt="NLW Unite" />
-      <nav className='flex gap-5'>
+      <nav className='flex items-center gap-5'>
         <a href="" className='text-sm font-medium text-zinc-300'>Eventos</a>
         <a href="" className='text-sm font-medium text-zinc-50'>Participantes</a>
       </nav>
