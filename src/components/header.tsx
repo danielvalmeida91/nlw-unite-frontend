3720f522f45cec2de7ae63ca5f785d918995aaf6
danielvalmeida91 <danielvalmeida91@gmail.com>
2024-04-04 00:19:27 -0300

    wip : aula 02 21:48

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,13 @@
 import nlwUniteLogo from '../assets/nlw-unite-icon.svg'
+import { NavLink } from './nav-link'
 export function Header(){
 
   return (
     <div className='flex items-center gap-5 py-2'>
       <img src={nlwUniteLogo} alt="NLW Unite" />
       <nav className='flex items-center gap-5'>
-        <a href="" className='text-sm font-medium text-zinc-300'>Eventos</a>
-        <a href="" className='text-sm font-medium text-zinc-50'>Participantes</a>
+        <NavLink href='/eventos'>Eventos</NavLink>
+        <NavLink href='/participantes'>Participantes</NavLink>
       </nav>
     </div>
   )
---
+++ src/components/icon-button.tsx
@@ -0,0 +1,3 @@
+// TODO criar um componente que recebera as informacoes do component button ja criado nas outras telas ,
+// como temos dois tipos de botoes deve-se criar uma variant transparent que atendera aos botoes de dentro
+// do formulario e uma variant solid que atendera aos botoes de fora do formulario
