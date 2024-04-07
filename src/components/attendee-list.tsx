9c7ef31ff08e808e8ac4ec8f5fb934b675cd43d3
danielvalmeida91 <danielvalmeida91@gmail.com>
2024-04-07 10:13:02 -0300

    finished nlw

--- src/components/attendee-list.tsx
+++ src/components/attendee-list.tsx
@@ -4,29 +4,96 @@ import { Table } from './table/table'
 import { TableHeader } from './table/table-header'
 import { TableCell } from './table/table-cell'
 import { TableRow } from './table/table-row'
-import { attendees } from '../data/attendee'
 import { formatDistance } from 'date-fns'
 import { ptBR } from 'date-fns/locale'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+
+interface Attendee {
+  id: string
+  name: string
+  email: string
+  createdAt: string
+  checkedInAt: string | null
+}
 
 export function AttendeeList() {
-  const [page , setPage] = useState(1)
-  const totalPages = Math.ceil(attendees.length/10)
+  const [search, setSearch] = useState(() => {
+    const url = new URL(window.location.toString())
+
+    if(url.searchParams.has('search')) {
+      return url.searchParams.get('search') ?? ''
+    }
+
+    return ''
+  })
+  const [page , setPage] = useState(() => {
+    const url = new URL(window.location.toString())
+
+    if(url.searchParams.has('page')) {
+      return Number(url.searchParams.get('page'))
+    }
+
+    return 1
+  })
+  const [attendees, setAttendees] = useState<Attendee[]>([])
+  const [total, setTotal] = useState(0)
+  const totalPages = Math.ceil(total/10)
+
+  useEffect(() => {
+    const url = new URL('http://localhost:3333/events/9e9bd979-9d10-4915-b339-3786b1634f33/attendees')
+
+    url.searchParams.set('pageIndex', String(page - 1))
+
+    if(search.length > 0){
+      url.searchParams.set('query', search)
+    }
+    
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      setAttendees(data.attendees)
+      setTotal(data.total)
+    })
+  
+  }, [page, search])
+
+  const onSearchInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setCurrentSearch(event.target.value)
+    setCurrentPage(1)
+  }
+
+  const setCurrentPage = (page: number) => {
+    const url = new URL(window.location.toString())
+
+    url.searchParams.set('page', String(page))
+    window.history.pushState({}, '', url)
+
+    setPage(page)
+  }
+
+  const setCurrentSearch = (search: string) => {
+    const url = new URL(window.location.toString())
+
+    url.searchParams.set('search', search)
+    window.history.pushState({}, '', url)
+
+    setSearch(search)
+  }
 
   const handleNext = () => {
-    setPage(page => page + 1)
+    setCurrentPage(page + 1)
   }
 
   const handlePrev = () => {
-    setPage(page => page - 1)
+    setCurrentPage(page - 1)
   }
 
   const handleFirst = () => {
-    setPage(1)
+    setCurrentPage(1)
   }
 
   const handleLast = () => {
-    setPage(totalPages)
+    setCurrentPage(totalPages)
   }
   return (
     <div className='flex flex-col gap-4'>
@@ -34,7 +101,7 @@ export function AttendeeList() {
     <h1 className="text-2xl font-bold">Participantes</h1>
     <div className='px-3 w-72 py-1.5 border border-white/10 rounded-lg flex items-center gap-3' >
       <Search className='size-4 text-emerald-300' />
-      <input className="bg-transparent flex-1 outline-none border-0 p-0 text-sm" placeholder='Buscar participante ...'/>
+      <input className="bg-transparent flex-1 outline-none border-0 p-0 text-sm focus:ring-0" placeholder='Buscar participante ...' value={search} onChange={onSearchInputChange}/>
     </div>
   </div>
  
@@ -52,8 +119,8 @@ export function AttendeeList() {
       </tr>
     </thead>
     <tbody>
-      {attendees.slice((page - 1) * 10, page * 10).map((attendee) => {
-        const { id, name, email, createdAt, checkInAt } = attendee
+      {attendees.map((attendee) => {
+        const { id, name, email, createdAt, checkedInAt } = attendee
         return(
           <TableRow key={id}>
             <TableCell>
@@ -66,8 +133,12 @@ export function AttendeeList() {
                 <span>{email}</span>
               </div>
             </TableCell>
-            <TableCell>{formatDistance(createdAt, new Date(), { locale: ptBR, addSuffix: true })}</TableCell>
-            <TableCell>{formatDistance(checkInAt, new Date(), { locale: ptBR, addSuffix: true })}</TableCell>
+            <TableCell>{formatDistance(createdAt, new Date().toISOString(), { locale: ptBR, addSuffix: true })}</TableCell>
+            <TableCell>{
+              checkedInAt 
+              ? formatDistance(checkedInAt, new Date().toISOString(), { locale: ptBR, addSuffix: true }) 
+              : <span className='text-zinc-400'>Não fez check-in</span>
+            }</TableCell>
             <TableCell>
               <IconButton className='bg-black/20 border border-white/10 rounded-md p-1.5'>
                 <MoreHorizontal className='size-4' />
@@ -82,12 +153,12 @@ export function AttendeeList() {
     <tfoot>
       <tr>
         <TableCell colSpan={3} >
-          {`Mostrando ${page * 10} de ${attendees.length} itens`}
+          {`Mostrando ${attendees.length} de ${total} ${attendees.length > 1 ? 'itens' : 'item'}`}
         </TableCell>
         <TableCell className='text-right' colSpan={3}>
           <div className='inline-flex items-center gap-8'>
             <span>
-              {`Página ${page ?? '-'} de ${Math.ceil(attendees.length/10)}`}
+              {`Página ${page ?? '-'} de ${totalPages}`}
             </span>
 
             <div className='flex gap-1.5'>
